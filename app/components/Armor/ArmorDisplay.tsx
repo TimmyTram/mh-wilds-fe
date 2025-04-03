96374a2e3ad7450788357248325cda471af7ac60
Timmy Tram <74998146+TimmyTram@users.noreply.github.com>
2025-04-03 14:00:06 -0700

    temp fix till they fix api

--- app/components/Armor/ArmorDisplay.tsx
+++ app/components/Armor/ArmorDisplay.tsx
@@ -11,7 +11,8 @@ const ArmorDisplay = ({ armorSet }: ArmorDisplayProps) => {
         <Link href={`/armor/${armorSet.id}`}>
             <div className="flex flex-col items-center justify-center p-4 border rounded-lg shadow-md min-w-[100px]">
                 <div className="flex flex-col items-center justify-center mb-4">
-                    <h2 className="text-red-500 text-center flex items-center justify-center">{armorSet.name}</h2>
+                    {/* For some reason the API does not return the name of the armorset in other languages?*/}
+                    {armorSet.name || (armorSet.pieces[0] && armorSet.pieces[0].name)}
                 </div>
             </div>
         </Link>
