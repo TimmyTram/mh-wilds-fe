55d8f160a5e0b98217451569583fce5b0ee1d1a3
Timmy Tram <74998146+TimmyTram@users.noreply.github.com>
2025-04-26 10:00:23 -0700

    charm img

--- app/components/MonsterHunter/Charms/CharmCard.tsx
+++ app/components/MonsterHunter/Charms/CharmCard.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { formatCharmSetName } from "@/app/utils/utils";
+import Image from "next/image";
 
 interface CharmCardProps {
     id: number | undefined;
@@ -11,6 +12,13 @@ const CharmCard = ({ id, name }: CharmCardProps) => {
         <Link href={`/charms/${id}`} className="w-[200px]">
             <div className="flex flex-col items-center justify-center p-4 border rounded-lg shadow-md w-[200px] h-[280px]">
                 <div className="flex flex-col items-center justify-center mb-4">
+                    <Image
+                        src={`/assets/ArmorIcons/charm_filled.png`}
+                        alt="charm"
+                        width={64}
+                        height={64}
+                        className="mb-2"
+                    />
                     <p>{formatCharmSetName(name)}</p>
                 </div>
             </div>
