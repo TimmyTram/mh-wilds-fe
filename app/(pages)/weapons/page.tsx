0a02471734d06c2905020410e6abc3fb895c6fc9
Timmy Tram <74998146+TimmyTram@users.noreply.github.com>
2025-04-27 14:19:12 -0700

    (Weapons): Created query/projection for weapons of a specific kind

--- app/(pages)/weapons/page.tsx
+++ app/(pages)/weapons/page.tsx
@@ -1,4 +1,22 @@
+'use client';
+
+import { useLanguageContext } from "@/app/Context/LanguageProvider";
+import useFetchAllMhData from "@/app/hooks/fetchAllMhData";
+import { Weapon } from "@/app/types/api/weapons/Weapon";
+import { useState } from "react";
+
 const Page = () => {
+    const [kind, setKind] = useState("bow");
+    const { language, isLanguageLoaded } = useLanguageContext();
+    const { data, loading, error } = useFetchAllMhData<Weapon>(
+        `weapons?q={"kind":"${kind}"}&p={"id": true,"name": true,"crafting.row": true,"crafting.column": true}`,
+        isLanguageLoaded ? language : undefined
+      );
+      
+
+    console.log(data);
+
+
     return (
         <div>
             this page is WIP
